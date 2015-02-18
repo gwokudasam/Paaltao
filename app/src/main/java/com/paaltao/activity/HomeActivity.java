a2d275de781a78e30980b22b153de1391610c74a
Arindam Dawn <arindamdawn3@gmail.com>
2015-02-18 06:14:38 +0530

    Few tweaks added.

--- app/src/main/java/com/paaltao/activity/HomeActivity.java
+++ app/src/main/java/com/paaltao/activity/HomeActivity.java
@@ -13,6 +13,8 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -45,6 +47,7 @@ public class HomeActivity extends ActionBarActivity implements MaterialTabListen
 
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.app_bar);
         toolbar.setTitleTextColor(Color.WHITE);
+        this.setTitle("you are awesome!");
         this.setSupportActionBar(toolbar);
 
 //        floating action menu
@@ -165,10 +168,27 @@ public class HomeActivity extends ActionBarActivity implements MaterialTabListen
 
     }
 
-
-    public void productClick() {
-        startActivity(new Intent(getApplicationContext(), ProductDetailsActivity.class));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_product_details, menu);
+        return true;
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.cart_icon) {
+            Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
 }
